a6d4a70d6ce947d7bdcb31bbc7ec791a84af43b7
Kudzai Gopfa <kudzaigopfa3@gmail.com>
2023-01-10 21:50:09 +0000

    ADD header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
+import Header from '../components/Header';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont,
+      "Segoe UI", "Roboto", "Oxygen","Ubuntu",
+      "Cantarell", "Fira Sans", "Droid Sans",
+      "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
