5071ebf881d6449b1b32bcf151df44d2f8e30b04
Kudzai Gopfa <kudzaigopfa3@gmail.com>
2023-01-15 11:15:18 +0000

    ADD SSR to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
-import { Inter } from '@next/font/google'
-import Questions from './questions'
+import { Inter } from '@next/font/google';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import Questions, { getServerSideProps as getServerSidePropsQuestions } from './questions';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  return getServerSidePropsQuestions(context);
+}
+
+export default function Home(props: any) {
   return (
-    <Questions />
+    <Questions {...props}/>
   )
 }
